e6e7c831fae6013dcc32789c4e21700379a42272
Pedro Rigon <pedrohenriquecasarotto@hotmail.com>
2022-04-26 19:26:00 -0300

    Testei a Trie

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ import csv
 import re
 
 CsvtoBin()
+
+
+trie1 = Trie()
+trie1.insert("Brenda Galinha", 2001)
+trie1.insert("Bruna", 2002)
+trie1.insert("Pedro Bor", 2001)
+
+print(trie1.starts_with("Br"))
