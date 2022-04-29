2682c13454f31ae21ab5e99eb9db6b1fe06c4c82
Pedro Rigon <pedrohenriquecasarotto@hotmail.com>
2022-04-29 04:50:20 -0300

    alterei os arquivos

--- CsvConverterBinary.py
+++ CsvConverterBinary.py
@@ -238,126 +238,102 @@ def CsvtoBin():
              errors='ignore', encoding="UTF-8")
     TitleDataCPD = []
     nd1 = 0
-    if os.path.isfile(r"src\bin\TitleDataCPD.bin") == False:
-        with open(r"src\bin\TitleDataCPD.bin", "wb") as handler_bin1:
-            arquivo_csv = csv.reader(f, dialect='excel', delimiter=",")
-            for t, line in enumerate(arquivo_csv):
-                if t == 0:
-                    del line
-                else:
-                    i = 1
-                    for x in line[1::1]:
-                        i += 1
-                    xbin = TitleCPD(nd1, line)
-                    TitleDataCPD.append(xbin)
-                    pickle.dump(xbin, handler_bin1, pickle.HIGHEST_PROTOCOL)
-                    nd1 += 1
-        handler_bin1.close()
+    arquivo_csv = csv.reader(f, dialect='excel', delimiter=",")
+    for t, line in enumerate(arquivo_csv):
+        if t == 0:
+            del line
+        else:
+            i = 1
+            for x in line[1::1]:
+                i += 1
+            xbin = TitleCPD(nd1, line)
+            TitleDataCPD.append(xbin)  
+            nd1 += 1
     f.close()
 
     f = open(r"src\csv\LanguageDataCPD.csv", 'r',
              errors='ignore', encoding="UTF-8")
     LanguageDataCPD = []
     nd2 = 0
-    if os.path.isfile(r"src\bin\LanguageDataCPD.bin") == False:
-        with open(r"src\bin\LanguageDataCPD.bin", "wb") as handler_bin:
-            arquivo_csv = csv.reader(f, dialect='excel', delimiter=",")
-            for t, line in enumerate(arquivo_csv):
-                if t == 0:
-                    del line
-                else:
-                    i = 1
-                    for x in line[1::1]:
-                        i += 1
-                    xbin = LanguageCPD(nd2, line)
-                    LanguageDataCPD.append(xbin)
-                    pickle.dump(xbin, handler_bin, pickle.HIGHEST_PROTOCOL)
-                    nd2 += 1
-        handler_bin.close()
+    arquivo_csv = csv.reader(f, dialect='excel', delimiter=",")
+    for t, line in enumerate(arquivo_csv):
+        if t == 0:
+            del line
+        else:
+            i = 1
+            for x in line[1::1]:
+                i += 1
+            xbin = LanguageCPD(nd2, line)
+            LanguageDataCPD.append(xbin)
+            nd2 += 1
     f.close()
 
     f = open(r"src\csv\StartYearDataCPD.csv", 'r',
              errors='ignore', encoding="UTF-8")
     StartYearDataCPD = []
     nd3 = 0
-    if os.path.isfile(r"src\bin\StartYearDataCPD.bin") == False:
-        with open(r"src\bin\StartYearDataCPD.bin", "wb") as handler_bin:
-            arquivo_csv = csv.reader(f, dialect='excel', delimiter=",")
-            for t, line in enumerate(arquivo_csv):
-                if t == 0:
-                    del line
-                else:
-                    i = 1
-                    for x in line[1::1]:
-                        i += 1
-                    xbin = StartYearCPD(nd3, line)
-                    StartYearDataCPD.append(xbin)
-                    pickle.dump(xbin, handler_bin, pickle.HIGHEST_PROTOCOL)
-                    nd3 += 1
-        handler_bin.close()
+    arquivo_csv = csv.reader(f, dialect='excel', delimiter=",")
+    for t, line in enumerate(arquivo_csv):
+        if t == 0:
+            del line
+        else:
+            i = 1
+            for x in line[1::1]:
+                i += 1
+            xbin = StartYearCPD(nd3, line)
+            StartYearDataCPD.append(xbin)
+            nd3 += 1
     f.close()
 
     f = open(r"src\csv\TypeDataCPD.csv", 'r',
              errors='ignore', encoding="UTF-8")
     TypeDataCPD = []
     nd4 = 0
-    if os.path.isfile(r"src\bin\TypeDataCPD.bin") == False:
-        with open(r"src\bin\TypeDataCPD.bin", "wb") as handler_bin:
-            arquivo_csv = csv.reader(f, dialect='excel', delimiter=",")
-            for t, line in enumerate(arquivo_csv):
-                if t == 0:
-                    del line
-                else:
-                    i = 1
-                    for x in line[1::1]:
-                        i += 1
-                    xbin = TypeCPD(nd4, line)
-                    TypeDataCPD.append(xbin)
-                    pickle.dump(xbin, handler_bin, pickle.HIGHEST_PROTOCOL)
-                    nd4 += 1
-        handler_bin.close()
+    arquivo_csv = csv.reader(f, dialect='excel', delimiter=",")
+    for t, line in enumerate(arquivo_csv):
+        if t == 0:
+            del line
+        else:
+            i = 1
+            for x in line[1::1]:
+                i += 1
+            xbin = TypeCPD(nd4, line)
+            TypeDataCPD.append(xbin)
+            nd4 += 1
     f.close()
 
     f = open(r"src\csv\CountryOriginDataCPD.csv",
              'r', errors='ignore', encoding="UTF-8")
     CountryOriginDataCPD = []
     nd5 = 0
-    if os.path.isfile(r"src\bin\CountryOriginDataCPD.bin") == False:
-        with open(r"src\bin\CountryOriginDataCPD.bin", "wb") as handler_bin:
-            arquivo_csv = csv.reader(f, dialect='excel', delimiter=",")
-            for t, line in enumerate(arquivo_csv):
-                if t == 0:
-                    del line
-                else:
-                    i = 1
-                    for x in line[1::1]:
-                        i += 1
-                    xbin = CountryOriginCPD(nd5, line)
-                    CountryOriginDataCPD.append(xbin)
-                    pickle.dump(xbin, handler_bin, pickle.HIGHEST_PROTOCOL)
-                    nd5 += 1
-        handler_bin.close()
+    arquivo_csv = csv.reader(f, dialect='excel', delimiter=",")
+    for t, line in enumerate(arquivo_csv):
+        if t == 0:
+            del line
+        else:
+            i = 1
+            for x in line[1::1]:
+                i += 1
+            xbin = CountryOriginCPD(nd5, line)
+            CountryOriginDataCPD.append(xbin)
+            nd5 += 1
     f.close()
 
     f = open(r"src\csv\PopularRankDataCPD.csv",
              'r', errors='ignore', encoding="UTF-8")
     PopularRankDataCPD = []
     nd6 = 0
-    if os.path.isfile(r"src\bin\PopularRankDataCPD.bin") == False:
-        with open(r"src\bin\PopularRankDataCPD.bin", "wb") as handler_bin:
-            arquivo_csv = csv.reader(f, dialect='excel', delimiter=",")
-            for t, line in enumerate(arquivo_csv):
-                if t == 0:
-                    del line
-                else:
-                    i = 1
-                    for x in line[1::1]:
-                        i += 1
-                    xbin = PopularRankCPD(nd6, line)
-                    PopularRankDataCPD.append(xbin)
-                    pickle.dump(xbin, handler_bin, pickle.HIGHEST_PROTOCOL)
-                    nd6 += 1
-        handler_bin.close()
+    arquivo_csv = csv.reader(f, dialect='excel', delimiter=",")
+    for t, line in enumerate(arquivo_csv):
+        if t == 0:
+            del line
+        else:
+            i = 1
+            for x in line[1::1]:
+                i += 1
+            xbin = PopularRankCPD(nd6, line)
+            PopularRankDataCPD.append(xbin)
+            nd6 += 1
     f.close()
 
     Trie_mkfile_Title(TitleDataCPD)
